c7c332eac5df7f5c4d5a1e948ab2e65f56dfa8d8
Aman Agrawal <31407164+doubleCradle@users.noreply.github.com>
2024-11-23 20:21:03 +0530

    Update graph.h

--- Graph/Chess/src/graph.h
+++ Graph/Chess/src/graph.h
@@ -66,3 +66,15 @@ void graph_init(Node board[BOARD_SIZE][BOARD_SIZE]) {
         }
     }
 }
+
+
+void printNeighbors(Node* node) {
+    //printf("Neighbors of (%d, %d):\n", node->x, node->y);
+    printf("\nNeighbors of (%c%d):\n", node->name, node->y+1);
+    for (int i = 0; i < 8; i++) {
+        if (node->neighbors[i] != NULL) {
+           /* printf("  (%d, %d, %c)\n", node->neighbors[i]->x, node->neighbors[i]->y, node->neighbors[i]->name); */
+	      printf("(%c%d)\n", node->neighbors[i]->name, (node->neighbors[i]->y+1));
+        }
+    }
+}
