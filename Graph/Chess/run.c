3dd4ca970b4b201b3231a5f202a784e61b6bea9e
doubleCradle <iamagrawalaman@gmail.com>
2024-11-25 12:12:54 +0530

    add SDL2 support to render pieces

--- Graph/Chess/run.c
+++ Graph/Chess/run.c
@@ -1,22 +1,28 @@
 
 #include<SDL2/SDL.h>
 #include<stdio.h>
-#include"src/graph.h"
+#include"src/graph2.h"
+#include<SDL2/SDL_image.h>
 
 #define BOARD_SIZE 8
 
-void renderBoard(SDL_Renderer* renderer, Node board[BOARD_SIZE][BOARD_SIZE]) {
+void renderBoard(SDL_Renderer* renderer, NP board[BOARD_SIZE][BOARD_SIZE]) {
     for (int y = 0; y < BOARD_SIZE; y++) {
         for (int x = 0; x < BOARD_SIZE; x++) {
-            Node* node = &board[y][x];
+            NP* np = &board[y][x];
             // Set the color based on the square's position
-            if ((node->x + node->y) % 2 == 0) {
+            if ((np->node.x + np->node.y) % 2 == 0) {
                 SDL_SetRenderDrawColor(renderer, 240, 240, 240, 255); // Light color
             } else {
                 SDL_SetRenderDrawColor(renderer, 96, 59, 42, 255); // Dark color
             }
             // Render the square
-            SDL_RenderFillRect(renderer, &node->rect);
+            SDL_RenderFillRect(renderer, &np->node.rect);
+
+            // Render the piece if it's not empty
+            if (!np->piece.empty && np->piece.texture != NULL) {
+                SDL_RenderCopy(renderer, np->piece.texture, NULL, &np->node.rect);
+            }
         }
     }
 }
@@ -25,10 +31,9 @@ void renderBoard(SDL_Renderer* renderer, Node board[BOARD_SIZE][BOARD_SIZE]) {
 
 int main(int argc, char* argv[]){
 
-Node board[BOARD_SIZE][BOARD_SIZE];
-graph_init(board);
-//printNeighbors(&board[1][1]);
+NP board[BOARD_SIZE][BOARD_SIZE];
 SDL_Init(SDL_INIT_VIDEO); // if this returns 0, all ok. Else some error posted by SDL_GetError().
+IMG_Init(IMG_INIT_PNG);
 SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 400, 400, SDL_WINDOW_SHOWN);
 
 SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
@@ -38,15 +43,58 @@ SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED
         SDL_Quit();
       return 1;
  }
+graph_init(board, renderer);
+printNeighbors(&board[1][1]);
+
+
 
 int running = 1;
 SDL_Event event;
+int selectedX = -1, selectedY = -1;
 
 while(running){
 	while(SDL_PollEvent(&event) != 0){
 		if(event.type == SDL_QUIT) {
 			running = 0;
-		}
+		}else if (event.type == SDL_MOUSEBUTTONDOWN) {
+                int mouseX, mouseY;
+                SDL_GetMouseState(&mouseX, &mouseY);
+
+                // Calculate the board coordinates
+                int boardX = mouseX / 50; // Assuming each square is 50x50
+                int boardY = mouseY / 50;
+
+                // Check if the click is within the board boundaries
+                if (boardX >= 0 && boardX < BOARD_SIZE && boardY >= 0 && boardY < BOARD_SIZE) {
+                    NP* clickedSquare = &board[boardY][boardX];
+
+                    if (!clickedSquare->piece.empty) {
+                        // Select the piece
+                        if (selectedX == -1 && selectedY == -1) {
+                            selectedX = boardX;
+                            selectedY = boardY;
+                            printf("Selected piece at (%d, %d)\n", selectedX, selectedY);
+                        } else {
+                            // Move the piece to the new square
+                            if (selectedX != boardX || selectedY != boardY) {
+                                // Move the piece logic (you may want to add more checks here)
+                                board[boardY][boardX].piece = board[selectedY][selectedX].piece;
+                                board[selectedY][selectedX].piece.empty = 1; // Mark the old square as empty
+                                selectedX = -1; // Reset selection
+                                selectedY = -1;
+                            }
+                        }
+                    } else if (selectedX != -1 && selectedY != -1) {
+                        // If an empty square is clicked, move the selected piece there
+                        board[boardY][boardX].piece = board[selectedY][selectedX].piece;
+                        board[selectedY][selectedX].piece.empty = 1; // Mark the old square as empty
+                        selectedX = -1; // Reset selection
+                        selectedY = -1;
+                    }
+                }
+            }
+
+            //SDL_Delay(16);
 	}
 
 	
--- Graph/Chess/src/graph.h
+++ Graph/Chess/src/graph.h
@@ -1,80 +1,114 @@
 #include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+
+#define BOARD_SIZE 8
+
+//typedef struct Piece;
+
+typedef struct Piece {
+    char type;
+    int color;
+    int empty;
+    char* path;
+    SDL_Texture* texture;
+} Piece;
 
 typedef struct Node {
     char name;
     int x; 
     int y; 
     SDL_Rect rect;
+    //Piece* piece;
     struct Node* neighbors[8]; 
 } Node;
 
-#define BOARD_SIZE 8
+typedef struct NP {
+    Node node;
+    Piece piece;
+} NP;
 
-void graph_init(Node board[BOARD_SIZE][BOARD_SIZE]) {
+
+
+void graph_init(NP board[BOARD_SIZE][BOARD_SIZE], SDL_Renderer* renderer) {
  char alphabet[] = {'a','b','c','d','e','f','g','h'};
     for (int y = 0; y < BOARD_SIZE; y++) {
         for (int x = 0; x < BOARD_SIZE; x++) {
-            board[y][x].x = x;
-            board[y][x].y = y;
+            board[y][x].node.x = x;
+            board[y][x].node.y = y;
 
-	    board[y][x].rect.x = x*50;
-	    board[y][x].rect.y = y*50;
-	    board[y][x].rect.w = 50;
-	    board[y][x].rect.h = 50;
-	    board[y][x].name = alphabet[x];
+	    board[y][x].node.rect.x = x*50;
+	    board[y][x].node.rect.y = y*50;
+	    board[y][x].node.rect.w = 50;
+	    board[y][x].node.rect.h = 50;
+	    board[y][x].node.name = alphabet[x];
+        board[y][x].piece.empty = 1;
 
+	    board[1][x].piece.empty = 0;
+	    board[1][x].piece.color = 0;
+	    board[1][x].piece.path = "bishop.png";
+
+        if(!board[y][x].piece.empty){
+
+            SDL_Surface* imageSurface = IMG_Load(board[y][x].piece.path);
+            if (imageSurface == NULL) {
+                fprintf(stderr, "Failed to load image: %s\n", IMG_GetError());
+                continue; // Skip if the image fails to load
+            }
+            board[y][x].piece.texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
+            SDL_FreeSurface(imageSurface);
+        }
             // Initialize neighbors
             int neighborCount = 0;
 
             // Up
             if (y > 0) {
-                board[y][x].neighbors[neighborCount++] = &board[y - 1][x]; // North
+                board[y][x].node.neighbors[neighborCount++] = &board[y - 1][x].node; // North
             }
             // Down
             if (y < BOARD_SIZE - 1) {
-                board[y][x].neighbors[neighborCount++] = &board[y + 1][x]; // South
+                board[y][x].node.neighbors[neighborCount++] = &board[y + 1][x].node; // South
             }
             // Left
             if (x > 0) {
-                board[y][x].neighbors[neighborCount++] = &board[y][x - 1]; // West
+                board[y][x].node.neighbors[neighborCount++] = &board[y][x - 1].node; // West
             }
             // Right
             if (x < BOARD_SIZE - 1) {
-                board[y][x].neighbors[neighborCount++] = &board[y][x + 1]; // East
+                board[y][x].node.neighbors[neighborCount++] = &board[y][x + 1].node; // East
             }
             // Top-Left
             if (x > 0 && y > 0) {
-                board[y][x].neighbors[neighborCount++] = &board[y - 1][x - 1]; // Northwest
+                board[y][x].node.neighbors[neighborCount++] = &board[y - 1][x - 1].node; // Northwest
             }
             // Top-Right
             if (x < BOARD_SIZE - 1 && y > 0) {
-                board[y][x].neighbors[neighborCount++] = &board[y - 1][x + 1]; // Northeast
+                board[y][x].node.neighbors[neighborCount++] = &board[y - 1][x + 1].node; // Northeast
             }
             // Bottom-Left
             if (x > 0 && y < BOARD_SIZE - 1) {
-                board[y][x].neighbors[neighborCount++] = &board[y + 1][x - 1]; // Southwest
+                board[y][x].node.neighbors[neighborCount++] = &board[y + 1][x - 1].node; // Southwest
             }
             // Bottom-Right
             if (x < BOARD_SIZE - 1 && y < BOARD_SIZE - 1) {
-                board[y][x].neighbors[neighborCount++] = &board[y + 1][x + 1]; // Southeast
+                board[y][x].node.neighbors[neighborCount++] = &board[y + 1][x + 1].node; // Southeast
             }
 
             // Set remaining neighbors to NULL
             for (int i = neighborCount; i < 8; i++) {
-                board[y][x].neighbors[i] = NULL;
+                board[y][x].node.neighbors[i] = NULL;
             }
         }
     }
 }
 
 
-void printNeighbors(Node* node) {
+void printNeighbors(NP* np) {
     //printf("Neighbors of (%d, %d):\n", node->x, node->y);
-    printf("\nNeighbors of (%c%d):\n", node->name, node->y+1);
+    printf("\nNeighbors of (%c%d):\n", np->node.name, np->node.y+1);
     for (int i = 0; i < 8; i++) {
-        if (node->neighbors[i] != NULL) {
+        if (np->node.neighbors[i] != NULL) {
            /* printf("  (%d, %d, %c)\n", node->neighbors[i]->x, node->neighbors[i]->y, node->neighbors[i]->name); */
-	      printf("(%c%d)\n", node->neighbors[i]->name, (node->neighbors[i]->y+1));
+	      printf("(%c%d)\n", np->node.neighbors[i]->name, (np->node.neighbors[i]->y+1));
         }
     }
 }
